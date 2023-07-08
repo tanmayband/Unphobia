313fbab8235bdd8cf8a14dd86c28e48068887547
Tanmay Band <tanmayband@gmail.com>
2023-07-08 12:32:43 +0530

    Basic ghost movement

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -5,16 +5,35 @@ using UnityEngine.InputSystem;
 
 public class GhostController : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Rigidbody2D ghostBody;
+    [SerializeField]
+    private float ghostSpeed = 5f;
+
+    private CustomInput customInputActions;
+
+    private void Awake()
+    {
+        customInputActions = new CustomInput();
+        customInputActions.Ghost.Enable();
+
+        customInputActions.Ghost.Movement.performed += Move;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = customInputActions.Ghost.Movement.ReadValue<Vector2>();
+        ghostBody.velocity = inputVector * ghostSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
     }
 }
